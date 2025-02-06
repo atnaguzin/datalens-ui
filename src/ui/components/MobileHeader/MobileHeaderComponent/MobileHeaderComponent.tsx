9f1158b1bfdb958bfbff9dc00a6c953696a753bf
Kirill Atnaguzin <atnaguzin@gmail.com>
2025-02-06 18:18:44 +0300

    Grammar fixes, add icons for menu items and add mobile menu
    Signed-off-by: Kirill Atnaguzin <atnaguzin@gmail.com>

--- src/ui/components/MobileHeader/MobileHeaderComponent/MobileHeaderComponent.tsx
+++ src/ui/components/MobileHeader/MobileHeaderComponent/MobileHeaderComponent.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import type {MobileHeaderProps} from '@gravity-ui/navigation';
+import {Persons, ListCheck, Tags} from '@gravity-ui/icons';
+import type {MobileHeaderProps, MobileMenuItem} from '@gravity-ui/navigation';
 import {MobileHeader, getMobileHeaderCustomEvent} from '@gravity-ui/navigation';
 import block from 'bem-cn-lite';
 import {I18n} from 'i18n';
+import {useLocation} from 'react-router-dom';
 import {PRODUCT_NAME} from 'ui/constants';
 import type {MobileHeaderComponentProps} from 'ui/registry/units/common/types/components/MobileHeaderComponent';
 
@@ -22,22 +24,20 @@ const i18n = I18n.keyset('component.aside-header.view');
 
 const b = block('mobile-header');
 
-const menuItems = [
-    {
-        id: 'collections',
-        title: i18n('switch_collections'),
-        icon: iconCollection,
-        link: '/collections',
-        closeMenuOnClick: true,
-    },
-];
+const COLLECTIONS_PATH = '/collections';
+const PROJECTS_PATH = '/admin/projects';
+const ROLES_PATH = '/admin/roles';
+const USERS_PATH = '/admin/users';
+
+export const ITEMS_NAVIGATION_DEFAULT_SIZE = 18;
 
 enum Panel {
     User = 'user',
 }
 
-export const MobileHeaderComponent = ({renderContent, logoIcon}: MobileHeaderComponentProps) => {
+export const MobileHeaderComponent = ({renderContent, superUser, logoIcon}: MobileHeaderComponentProps) => {
     const ref = React.useRef<HTMLDivElement>(null);
+    const {pathname} = useLocation();
 
     const sideItem = (
         <div
@@ -60,6 +60,46 @@ export const MobileHeaderComponent = ({renderContent, logoIcon}: MobileHeaderCom
               },
           ]
         : undefined;
+
+    const menuItems: MobileMenuItem[] = React.useMemo(
+        ()=> [
+            {
+                id: 'collections',
+                title: i18n('switch_collections'),
+                icon: iconCollection,
+                iconSize: ITEMS_NAVIGATION_DEFAULT_SIZE,
+                current: pathname.includes(COLLECTIONS_PATH),
+                closeMenuOnClick: true,
+            },
+            ...(superUser?.isMaster ? [
+                {
+                    id: 'users',
+                    title: i18n('switch_service-users'),
+                    icon: Persons,
+                    iconSize: ITEMS_NAVIGATION_DEFAULT_SIZE,
+                    current: pathname.includes(USERS_PATH),
+                    closeMenuOnClick: true,
+                },
+                {
+                    id: 'projects',
+                    title: i18n('switch_service-projects'),
+                    icon: ListCheck,
+                    iconSize: ITEMS_NAVIGATION_DEFAULT_SIZE,
+                    current: pathname.includes(PROJECTS_PATH),
+                    closeMenuOnClick: true,
+                },
+                {
+                    id: 'roles',
+                    title: i18n('switch_service-roles'),
+                    icon: Tags,
+                    iconSize: ITEMS_NAVIGATION_DEFAULT_SIZE,
+                    current: pathname.includes(ROLES_PATH),
+                    closeMenuOnClick: true,
+                }
+            ] : [])
+        ],
+        [pathname],
+    );
 
     return (
         <MobileHeader
